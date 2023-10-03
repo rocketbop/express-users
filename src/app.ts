7238b579ff622dc1df64c680a6c88ef3dd366bbe
Paul Byrne <paulgregorybyrne@gmail.com>
2023-10-03 13:50:52 +0200

    Get basic test setup

--- src/app.ts
+++ src/app.ts
@@ -5,10 +5,10 @@ const app: Express = express();
 app.use(cors()).use(express.json()).options('*', cors());
 
 app.post('/users', (req: Request, res: Response) => {
-    res.send({}).status(201);
+    res.status(201).send({});
 });
 app.get('/users', (req: Request, res: Response) => {
-    res.send([]).status(200);
+    res.status(200).send([]);
 });
 
 export default app;
--- test/index.test.ts
+++ test/index.test.ts
@@ -2,15 +2,25 @@ import { describe, expect, test } from '@jest/globals';
 import request from 'supertest';
 import app from './../src/app';
 
-describe('your code', () => {
-    test('it passes the tests', () => {
-        expect(true).toEqual(true);
+describe('POST /users', () => {
+    test.only('should create a new user and return a status code of 201', async () => {
+        const newUser = {
+            name: 'Bart Simpson',
+            email: 'bart@example.com',
+        };
+
+        const response = await request(app).post('/users').send(newUser);
+
+        expect(response.body).toEqual({})
+        expect(response.status).toBe(201);
     });
 });
 
 describe('GET /users', () => {
     test('it returns an empty list of users when there are no users', async () => {
         const response = await request(app).get('/users');
+
+        expect(response.body).toEqual([]);
         expect(response.status).toBe(200);
     });
 });
