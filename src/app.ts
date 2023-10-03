dcd0d476ca8586298bd5772a356909e145331b7a
Paul Byrne <paulgregorybyrne@gmail.com>
2023-10-03 13:23:45 +0200

    Split app from our server listen call

---
+++ src/app.ts
@@ -0,0 +1,16 @@
+import express, {Express, Request, Response} from 'express';
+import cors from 'cors';
+
+const app: Express = express();
+app.use(cors())
+  .use(express.json())
+  .options('*', cors());
+
+app.post('/users', (req: Request, res: Response) => {
+  res.send({}).status(201);
+});
+app.get('/users', (req: Request, res: Response) => {
+  res.send([]).status(200);
+});
+
+export default app;
